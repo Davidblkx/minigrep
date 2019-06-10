d118fbb5fbe122b0b874cb2d3cea38e4b69996e9
David Pires <developer@davidpires.pt>
2019-06-10 16:25:20 +0100

    added basic logic to read params

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::process;
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_name);
+
+    if let Err(e) = run(config) {
+        println!("App errir: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.file_name)?;
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    file_name: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments!");
+        }
+
+        let query = args[1].clone();
+        let file_name = args[2].clone();
+
+        Ok(Config { query, file_name })
+    }
 }
