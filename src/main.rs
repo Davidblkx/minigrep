c91faf2d0e5c1d7cc42a4b02d05d0dc41b58c3df
David Pires <developer@davidpires.pt>
2019-06-10 16:30:14 +0100

    move to external library

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+extern crate minigrep;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,37 +16,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_name);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("App errir: {}", e);
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<Error>> {
-    let mut f = File::open(config.file_name)?;
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_name: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments!");
-        }
-
-        let query = args[1].clone();
-        let file_name = args[2].clone();
-
-        Ok(Config { query, file_name })
     }
 }
